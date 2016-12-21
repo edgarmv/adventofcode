ed09606502df8d5f765a47c30fedd26f08323abd
Edgar Vedvk <edgar.vedvik@gmail.com>
2016-12-21 17:42:24 +0100

    day 14

---
+++ day14-1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import hashlib
+import re
+
+hash_key = "yjdafjpo"
+
+possible_keys = set()
+keys_found = 0
+keys = []
+
+for num in range(1000000):
+    keynum = (hash_key + str(num)).encode("utf-8")
+    h = hashlib.md5(keynum).hexdigest().lower()
+    match = re.search(r"([a-z0-9])\1\1", h) 
+
+    tmp = set()
+
+    for key in possible_keys:
+        if key[1] + 1000 < num:
+            continue
+
+        match5 = re.search(r"({})\1\1\1\1".format(key[0]), h)
+        if match5:
+            keys_found += 1
+            keys.append(key[1])
+        else:
+            tmp.add(key)
+
+    if keys_found >= 64:
+        print(sorted(keys)[63])
+        break
+
+    possible_keys = tmp
+
+    if match:
+        possible_keys.add((match.group()[0], num))
